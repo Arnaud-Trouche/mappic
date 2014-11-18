f49dff4ef670baa63c63c1dfe38a662adada8065
arn00d <arn00d@outlook.com>
2014-11-18 19:09:59 +0100

    Header + menu (material design ^^)
    History does not work
    Login+ register not created

--- frontend/public/javascripts/main.js
+++ frontend/public/javascripts/main.js
@@ -1,12 +1,43 @@
+function init(page){
+    var logged = localStorage.getItem("login");
+    var path = logged ? '/log/' : '/nolog/';
+
+    //Display header
+    $.get('templates'+path+"header.mst", function(template){
+        $('body').html(Mustache.render(template, {username: localStorage.getItem('username')}));
+        //load the header.js
+        $.get('javascripts'+path+'header.js', null);
+    });    
+
+    //Load the template for the current page
+    $.get('templates'+path+page+'.mst', function(template) {
+        var rendered = Mustache.render(template, {});
+        $('body').append(rendered);
+        //Load the .js for the current page
+        $.get('javascripts'+path+page+'.js', null);
+
+        $(".link").click(function(){
+            var link = this.id;
+            History.pushState({state:link}, link, "#"+link);
+            init(link);            
+        });
+    });    
+}
+
 $( document ).ready(function() {
-	//if logged
-	if(localStorage.getItem("login") == true){
-		$("#content").html("Loggé");
-	}else{
-    	$("#content").html("Pas loggé");
-    	//load default template
-    	
-	}
+    init($(location).attr('hash') == "" ? 'home' : $(location).attr('hash').substring(1));   
 
+    //MARCHE PAS
+    $(window).on("navigate", function (event, data) {
+        var direction = data.state.direction;
 
+            console.info("hello?");
+        if (direction == 'back') {
+            console.info("hello?");
+        }
+        if (direction == 'forward') {
+        // do something else
+        }
+    });
 });
+
